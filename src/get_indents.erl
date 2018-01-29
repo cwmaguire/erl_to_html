bbfa1823e62afb8bd50a2da1e4e0d0dce95f30a2
Chris Maguire <cwmaguire@gmail.com>
2018-01-28 21:14:30 -0800

    Remove comments

--- src/get_indents.erl
+++ src/get_indents.erl
@@ -15,13 +15,11 @@ add_line(_EmptyLine, Lines, _Data = "\n") ->
     Lines;
 add_line(Line, Lines, Data) ->
     {Taken, Rest} = string:take(string:replace(Data, "\t", "    "), " "),
-    %io:format(user, "~p: Taken = ~p, Rest = ~p~n", [Line, Taken, Rest]),
     case {Taken, Rest} of
         {_, []} ->
             Lines;
         {"", _} ->
             Lines;
         _ ->
-            %io:format(user, "Length of Taken: ~p~n", [length(Taken)]),
             Lines#{Line => length(Taken)}
     end.
