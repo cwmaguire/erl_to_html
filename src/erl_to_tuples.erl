ba51f2425e6339efe6ff5734caa630f42fffb5ca
Chris Maguire <cwmaguire@gmail.com>
2018-08-08 22:16:44 -0700

    WIP: converting to tuples, cont ...

--- src/erl_to_tuples.erl
+++ src/erl_to_tuples.erl
@@ -339,23 +339,23 @@ expr({'fun',Line,Body}) ->
         {function,Fun,Arity} ->
             [line(Line),
              function(Fun), parse_symbol(Line, '/'),
-     arity(Arity)];
+             arity(Arity)];
         {function,M,F,A} when is_atom(M), is_atom(F), is_integer(A) ->
             [line(Line),
              span(<<"module">>, a2b(M)), parse_symbol(Line, ':'),
-     function(F), parse_symbol(Line, '/'),
-     arity(A)];
+             function(F), parse_symbol(Line, '/'),
+             arity(A)];
         {function,M0,F0,A0} ->
             %% R15: fun M:F/A with variables.
             M = expr(M0),
             F = expr(F0),
             A = expr(A0),
             [line(Line),
-             span(<<"module">>, a2b(M)),
+             {Line, a2b(M), ?MODULE_COLOUR},
              parse_symbol(Line, ':'),
-     span("function", F),
+             {Line, a2b(F), ?TEXT_COLOUR},
              parse_symbol(Line, '/'),
-     span("arity", A)]
+             {Line, "arity", A}]
     end;
 expr({call,Line,Fun,Args}) ->
     %% N.B. If F an atom then call to local function or BIF, if F a
@@ -379,32 +379,38 @@ expr({bin,Line,BinElements}) ->
      map_separate(fun bin/1, BinElements), '>>'];
 expr({op,Line,Op,A}) ->
     [line(Line),
-     span("operator", atom_to_list(Op)), expr(A)];
+     {Line, a2b(Op), ?TEXT_COLOUR},
+     expr(A)];
 expr({op,Line,'==',L,R}) ->
     [line(Line),
-     expr(L), span(<<"double_equals">>, "=="), expr(R)];
+     expr(L),
+     {Line, <<"==">>, ?DOUBLE_EQUALS_COLOUR},
+     expr(R)];
 expr({op,Line,Op,L,R}) ->
     [line(Line),
-     expr(L), span("operator", atom_to_list(Op)), expr(R)];
+     expr(L),
+     {Line, a2b(Op), ?ATOM_COLOUR},
+     expr(R)];
 expr({remote, Line, {atom, _MLine, Module}, {atom, _FLine, Function}}) ->
     [line(Line),
-     span(<<"module">>, a2b(Module)), parse_symbol(Line, ':'),
-     function(Function)];
+     {Line, a2b(Module), ?MODULE_COLOUR},
+     parse_symbol(Line, ':'),
+     function(Line, Function)];
 expr({nil, Line}) ->
     [line(Line), parse_symbol(Line, '['),
      ']'];
 expr({var,Line,V}) ->
     [line(Line),
-     var(V)];
+     var(Line, V)];
 expr({integer,Line,I}) ->
     [line(Line),
-     integer(I)];
+     integer(Line, I)];
 expr({char,Line,C}) ->
     [line(Line),
-     span("char", [$$, C])];
+     {Line, <<$$, (list_to_binary(C))/binary>>, ?CHAR_COLOUR}];
 expr({float,Line,F}) ->
     [line(Line),
-     span("float", io_lib:format("~w", [F]))];
+     {Line, list_to_binary(hd(io_lib:format("~w", [F]))), ?FLOAT_COLOUR}];
 expr({atom,Line,A}) when A == ':';
                          A == '<<';
                          A == '>>';
@@ -424,17 +430,16 @@ expr({atom,Line,A}) when A == ':';
                          A == '(';
                          A == '"' ->
     io:format(user, "{atom, Line, ~p}", [A]),
-    Str = [$', atom_to_list(A), $'],
     [line(Line),
-     span("atom", list_to_binary(Str))];
+     {Line, <<$', (a2b(A))/binary, $'>>, ?ATOM_COLOUR}];
 expr({atom,Line,A}) ->
-[line(Line),
-     span("atom", atom_to_list(A))];
+    [line(Line),
+     {Line, a2b(A), ?ATOM_COLOUR}];
 expr({string,Line,S}) ->
     [line(Line),
      '"',
      %{no_parse, <<"--><!-- before parse_control_sequences(S) --><!--\n">>},
-     parse_control_sequences(S),
+     parse_control_sequences(Line, S),
      %{no_parse, <<"--><!-- after parse_control_sequences(S) --><!--\n">>},
      '"'];
 expr({tuple,Line,Exprs}) ->
@@ -467,74 +472,85 @@ expr(_Tail_ = {tail, {cons, Line, Head, Tail}}) ->
      [expr(Head), ',' | expr({tail, Tail})]];
 expr({tail, {var, Line, Expr}}) ->
     [line(Line),
-     var(Expr)];
+     var(Line, Expr)];
 expr({tail, Call = {call, Line, _Fun, _Args}}) ->
     [line(Line),
      expr(Call)];
 expr({tail, Unknown}) ->
     io:format(user, "Unknown tail ~p~n", [Unknown]);
-expr({record,Line,Name,exprFields}) ->
+expr({record,Line,Name,ExprFields}) ->
     [line(Line),
-     [span("record_hash", {no_parse, {no_parse, <<"#">>}}), span("record_name", atom_to_list(Name)), '{',
-      map_separate(fun expr_field/1, exprFields),
-      '}']];
+     [parse_symbol(Line, '#'),
+      {Line, a2b(Name), ?RECORD_NAME_COLOUR},
+      parse_symbol(Line, '{'),
+      map_separate(fun expr_field/1, ExprFields),
+      parse_symbol(Line, '}')]];
 expr({record_index,Line,Name,Field}) ->
     [line(Line),
-     [span("record", ['#', atom_to_list(Name)]), '.', expr(Field)]];
+     [parse_symbol(Line, '#'),
+      {Line, a2b(Name), ?RECORD_NAME_COLOUR},
+      parse_symbol(Line, '.'),
+      expr(Field)]];
 expr({record_field,Line,Expression,RecName,Field}) ->
     [line(Line),
-     [expr(Expression), '#', span("record", atom_to_list(RecName)), '.', expr(Field)]];
+     [expr(Expression),
+      parse_symbol(Line, '#'),
+      {Line, atom_to_list(RecName), ?RECORD_NAME_COLOUR},
+      parse_symbol(Line, '.'),
+      expr(Field)]];
 % How does this happen? (Foo).bar ?
 %expr({record_field,Line,Rec0,Field0}) ->
     %Rec1 = expr(Rec0),
     %Field1 = expr(Field0);
 expr(UnknownExpr) ->
     io:format(user, "Unknown expr:~n~p~n", [UnknownExpr]),
-    span("error").
+    {-1, <<"error">>, ?RED}.
 
 
-bit_type(Atom) when is_atom(Atom) ->
-    span(Atom);
-bit_type({Atom, Integer}) when is_atom(Atom), is_integer(Integer) ->
-    [atom(Atom), parse_symbol(Line, ':'),
-     integer(Integer)].
+bit_type(Line, Atom) when is_atom(Atom) ->
+    {Line, Atom, ?ATOM_COLOUR};
+bit_type(Line, {Atom, Integer}) when is_atom(Atom), is_integer(Integer) ->
+    [{Line, Atom, ?ATOM_COLOUR},
+     parse_symbol(Line, ':'),
+     {Line, Integer, ?INTEGER_COLOUR}].
 
 expr_field({record_field,Lf,{atom,_La,F},Expr}) ->
     [line(Lf),
      span(F), '=', expr(Expr)];
 expr_field({record_field,Lf,{var,_La,'_'},Expr}) ->
     [line(Lf),
-     parse_symbol(Line, '_'),
+     parse_symbol(Lf, '_'),
      '=', expr(Expr)].
 
 guard_group(GuardGroup) ->
     separate(lists:map(fun expr/1, GuardGroup)).
 
-fun_name({atom, _Line, Name}) ->
-    [%line(Line),
-     span(<<"fun_call">>, Name)];
-fun_name(_A = {remote, _LineR, {atom, _LineM, Module}, {atom, _LineF, Function}}) ->
+fun_name({atom, Line, Name}) ->
+    [line(Line),
+     {Line, Name, ?TEXT_COLOUR}];
+fun_name(_A = {remote, LineR, {atom, _LineM, Module}, {atom, LineF, Function}}) ->
      %io:format(user, "fun_name(~p)~n", [A]),
-    [%line(LineR),
+    [line(LineR),
      %line(LineM),
-     span(<<"remote_fun_module">>, a2b(Module)),
-     ':',
+     {LineR, a2b(Module), ?TEXT_COLOUR},
+     parse_symbol(LineR, ':'),
      %line(LineF),
-     span(<<"fun_call">>, a2b(Function))];
+     {LineF, a2b(Function), ?TEXT_COLOUR}];
 fun_name(Any) ->
     io:format(user, "Unknown fun_name: ~p~n", [Any]),
     [].
 
-parse_control_sequences(String) when is_list(String) ->
+parse_control_sequences(Line, String) when is_list(String) ->
     CtrlSeqs = [<<"~n">>,
                 <<"~p">>,
                 <<"~b">>,
                 <<"~w">>],
     Parsed = lists:foldl(fun pcs/2, [list_to_binary(String)], CtrlSeqs),
-    lists:map(fun span_bins/1, Parsed).
+    lists:map(fun(Bin) -> ctrl_seq_bin(Line, Bin) end, Parsed).
 
 pcs(_, []) ->
     [];
+% Where would a tuple come from?
 pcs(CtrlSeq, [Tuple | Rest]) when is_tuple(Tuple) ->
     [Tuple | pcs(CtrlSeq, Rest)];
 pcs(CtrlSeq, [Bin | Rest]) ->
@@ -545,10 +561,10 @@ pcs(CtrlSeq, [Bin | Rest]) ->
             lists:flatten([[Before, CtrlSeq | pcs(CtrlSeq, [After])] | pcs(CtrlSeq, Rest)])
     end.
 
-span_bins(CtrlSeq = <<$~, _>>) ->
-    span(<<"control_sequence">>, CtrlSeq);
-span_bins(String) ->
-    span(<<"string">>, String).
+ctrl_seq_bin(Line, CtrlSeq = <<$~, _>>) ->
+    {Line, CtrlSeq, ?CONTROL_SEQUENCE_COLOUR};
+ctrl_seq_bin(Line, String) ->
+    {Line, String, ?TEXT_COLOUR}.
 
 %% -type lc_bc_qual([Qualifier]) -> [Qualifier].
 %%  Allow filters to be both guard tests and general expressions.
@@ -574,39 +590,47 @@ type({type,Line,binary,[{_, _, M}, {_, _, 0}]}) ->
      parse_symbol(Line, '<<'),
      parse_symbol(Line, '_'),
      parse_symbol(Line, ':'),
-     span(<<"integer">>, i2b(M)), '>>'];
+     {Line, i2b(M), ?INTEGER_COLOUR},
+     parse_symbol(Line, '>>')];
 type({type,Line,binary,[{_, _, 0},{_, _, N}]}) ->
     [line(Line),
      parse_symbol(Line, '<<'),
      parse_symbol(Line, '_'),
      parse_symbol(Line, ':'),
      parse_symbol(Line, '_'),
-     '*', span(<<"integer">>, i2b(N)), '>>'];
+     parse_symbol(Line, '*'),
+     {Line, i2b(N), ?INTEGER_COLOUR},
+     parse_symbol(Line, '>>')];
 type({type,Line,binary,[{_, _, M},{_, _, N}]}) ->
     [line(Line),
      parse_symbol(Line, '<<'),
      parse_symbol(Line, '_'),
      parse_symbol(Line, ':'),
-     span(<<"integer">>, i2b(M)), ',',
+     {Line, i2b(M), ?INTEGER_COLOUR},
      parse_symbol(Line, '_'),
      parse_symbol(Line, ':'),
      parse_symbol(Line, '_'),
-     '*', span(<<"integer">>, i2b(N)), '>>'];
+     parse_symbol(Line, '*'),
+     {Line, i2b(N), ?INTEGER_COLOUR},
+     parse_symbol(Line, '>>')];
 type({type,Line,'fun',[]}) ->
-    [line(Line), span(<<"fun">>), parse_symbol(Line, '('),
-     ')'];
+    [line(Line),
+     parse_symbol(Line, 'fun'),
+     parse_symbol(Line, '('),
+     parse_symbol(Line, ')')];
 type({type,Line,'fun',[{type,TypeLine,any},B]}) ->
     [line(Line),
-     parse_symbol(Line, <<"fun">>),
-     span(<<"fun">>),
+     parse_symbol(Line, 'fun'),
      parse_symbol(Line, '('),
-     line(TypeLine), parse_symbol(Line, '('),
-     '...', parse_symbol(Line, ')'),
+     line(TypeLine),
+     parse_symbol(Line, '('),
+     parse_symbol(Line, '...'),
+     parse_symbol(Line, ')'),
      parse_symbol(Line, '->'),
      type(B), ')'];
 type({type,Line,'fun',[{type,Lt,product, ArgTypes},TypeResult]}) ->
     [line(Line),
-     span("fun"),
+     parse_symbol(Line, 'fun'),
      parse_symbol(Line, '('),
      line(Lt), parse_symbol(Line, '('),
      lists:join(',', [type(T) || T <- ArgTypes]),
@@ -618,16 +642,30 @@ type({type,Line,nil,[]}) ->
     [line(Line), parse_symbol(Line, '['),
      ']'];
 type({type,Line,range,[L,H]}) ->
-    [line(Line), type(L), '..', type(H)];
+    [line(Line),
+     type(L),
+     parse_symbol(Line, '..'),
+     type(H)];
 type({type,Line,map,any}) ->
-    [line(Line), span(<<"map_hash">>, "#"), '{', '}'];
+    [line(Line),
+     parse_symbol(Line, "#"),
+     parse_symbol(Line, '{'),
+     parse_symbol(Line, '}')];
 type({type,Line,map,Ps}) ->
     Ps1 = map_pair_types(Ps),
-    [line(Line), span(<<"map_hash">>, "#"), '{', Ps1, '}'];
+    [line(Line),
+     parse_symbol(Line, "#"),
+     parse_symbol(Line, '{'),
+     Ps1,
+     parse_symbol(Line, '}')];
 type({type,Line,record,[{atom,_La,N}|Fs]}) ->
     Fs1 = field_types(Fs),
     [line(Line),
-     span(<<"record_hash">>, <<"#">>), span(<<"record_name">>, a2b(N)), '{', Fs1, '}'];
+     {Line, <<"#">>, ?RECORD_HASH},
+     {Line, a2b(N), ?RECORD_NAME},
+     parse_symbol(Line, '{'),
+     Fs1,
+     parse_symbol(Line, '}')];
 
 %type({remote_type,Line,[{atom,Lm,M},{atom,Ln,N},As]}) ->
     %As1 = type_list(As),
@@ -644,17 +682,19 @@ type({type,Line,list,Ts}) when is_list(Ts) ->
     [line(Line), parse_symbol(Line, '['),
      lists:join(',', type_list(Ts)), ']'];
 type({type,Line,List,Ts}) when is_list(Ts) ->
-    [line(Line), span(a2b(List)), parse_symbol(Line, '('),
-     lists:join(',', type_list(Ts)), ')'];
+    [line(Line),
+     {Line, a2b(List), ?TEXT_COLOUR},
+     parse_symbol(Line, '('),
+     lists:join(parse_symbol(Line, ','), type_list(Ts)),
+     parse_symbol(Line, ')')];
 % TODO Figure out what V could be; I've only seen '_'
 type({var, Line, _V}) ->
-    [line(Line), '_'];
-%type({user_type,Line,N,As}) ->
-    %As1 = type_list(As),
-    %{user_type,Line,N,As1};
+    [line(Line), parse_symbol(Line, '_')];
 type({type,Line,Atom,[]}) when is_atom(Atom) ->
-    [line(Line), Atom, parse_symbol(Line, '('),
-     ')'];
+    [line(Line),
+     {Line, Atom, ?TEXT_COLOUR},
+     parse_symbol(Line, '('),
+     parse_symbol(Line, ')')];
 % TODO A record would fit this pattern, what else?
 type(UnknownType = {type,_Line,_N,_As}) ->
     %As1 = type_list(As),
@@ -667,12 +707,16 @@ map_pair_types(PairTypes) ->
     lists:join(',', [pair_type(PT) || PT <- PairTypes]).
 
 pair_type({type, Line, map_field_assoc, [K, V]}) ->
-    [line(Line), type(K), '=>', type(V)];
+    [line(Line), type(K), parse_symbol(Line, '=>'), type(V)];
 pair_type({type, Line, map_field_exact, [K, V]}) ->
     [line(Line), type(K), ':=', type(V)].
 
-field_types([{type,Line,field_type,[{atom,_La,A},T]}|Fs]) ->
-    [line(Line), A, '::', type(T) | field_types(Fs)];
+field_types([{type,Line,field_type,[{atom,La,A},T]}|Fs]) ->
+    L = line(Line),
+    Atom = {La, a2b(A), ?ATOM_COLOUR},
+    Symbol = parse_symbol(Line, '::'),
+    T = type(T),
+    [L, Atom, Symbol, T | field_types(Fs)];
 field_types([]) -> [].
 
 type_list([T|Ts]) ->
@@ -683,9 +727,12 @@ type_list([]) -> [].
 %% This is a list of generators _or_ filters
 %% which are simply expressions
 %% A generator is a target and a source
-lc_bc_qual({generate,_Line,Target,Source}) ->
+lc_bc_qual({generate,Line,Target,Source}) ->
     [parse_symbol(Line, '['),
-     expr(Target), '<-', expr(Source), ']'];
+     expr(Target),
+     parse_symbol(Line, '<-'),
+     expr(Source),
+     parse_symbol(Line, ']')];
 lc_bc_qual({b_generate,_Line,Target,Source}) ->
     [expr(Target), '<=', expr(Source)];
 lc_bc_qual(FilterExpression) ->
@@ -699,14 +746,17 @@ bin({bin_element,Line,Var,Size,MaybeTypes}) ->
              "";
          Int ->
              [parse_symbol(Line, ':'),
-     integer(Int)]
+              {Line, i2b(Int), ?TEXT_COLOUR}]
      end,
      case MaybeTypes of
          default ->
              "";
          _ ->
              [parse_symbol(Line, '/'),
-     map_separate('-', fun bit_type/1, MaybeTypes)]
+             Fun = fun(MaybeType) ->
+                       bit_type(Line, MaybeType)
+                   end,
+             map_separate('-', Fun, MaybeTypes)]
      end].
 
 %bin_element({bin_element,Line,Expression,Size1,Type1}) ->
@@ -748,31 +798,6 @@ i2b(I) ->
 
 a2b(A) ->
     list_to_binary(atom_to_list(A)).
-
-span(Keyword) ->
-    span(Keyword, Keyword).
-
-span(Class, Atom) when is_atom(Atom) ->
-    span(Class, a2b(Atom));
-span(_Class = <<"line">>, Line) ->
-    {line, Line};
-span(Class = <<"tuple">>, Text) ->
-     [{no_parse, <<"--><span class=\"">>},
-      Class,
-      {no_parse, <<"\"><!--\n">>},
-      Text,
-      {no_parse, <<"--></span><!--\n">>}];
-span(Class, Text) ->
-     [{no_parse, <<"--><span class=\"">>},
-      Class,
-      {no_parse, <<"\">">>},
-      Text,
-      {no_parse, <<"</span><!--\n">>}].
-
-%span_open(Class) ->
-    %["<span class=\"",
-     %Class,
-     %"</span>"].
 
 parse_symbol(Line, '(') ->
     {Line, <<"(">>, ?TEXT_COLOUR};
@@ -832,7 +857,6 @@ parse_symbol(Line, '_') ->
     {Line, <<"_">>, ?TEXT_COLOUR};
 parse_symbol(Line, '#') ->
     {Line, <<"#">>, ?TEXT_COLOUR};
-    span(<<"hash">>, <<"#">>);
 parse_symbol(Line, '|') ->
     {Line, <<"|">>, ?CONS_COLOUR};
 parse_symbol(Line, '=>') ->
@@ -845,11 +869,11 @@ parse_symbol(Line, eof) ->
     {Line, <<"eof">>, ?EOF_COLOUR};
 parse_symbol(Line, module) ->
     {Line, <<"module">>, ?MODULE_COLOUR};
-parse_symbol(Line, fun) ->
+parse_symbol(Line, 'fun') ->
     {Line, <<"fun">>, ?FUN_COLOUR};
 
 parse_symbol(Line, Atom) ->
-    span(atom_to_list(Atom)).
+    {Line, a2b(Atom), ?ATOM_COLOUR}.
 
 line(Line) ->
     {Line, i2b(Line), ?LINE_COLOUR}.
